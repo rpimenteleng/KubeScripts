243720d694c788596f5db167e51ea806da959a6b
Roberto Pimentel <roberto.pimentel.eng@gmail.com>
2019-06-07 09:42:33 -0400

    Create aks-force-context.sh

---
+++ aks-force-context.sh
@@ -0,0 +1,12 @@
+# Azure Context Utility
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "Usage:"
+    echo "   force-context [yourClusterName] [yourResourceGroupName]"
+    az aks list -o table
+    exit 1
+fi
+
+az aks get-credentials -n $1 -g $2
+kubectl config get-contexts
+kubectl config use-context $1
